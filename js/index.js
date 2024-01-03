67c222fee8024671a52f775b6985501ec4eb1a7f
Seongwon <sngoneee@gmail.com>
2024-01-03 11:12:45 +0900

    test

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   $('#fullpage').fullpage({         
     anchors: ['sec1', 'sec2', 'sec3', 'sec4'],
     menu: '#menu',
-    scrollingSpeed: 1000,
+    scrollingSpeed: 500,
     navigation: true,
     // responsiveWidth: 1025,
     navigationPosition: 'right',
